59c7a0cab4e5e861a2e673d1266b80fc6e0307ff
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-19 22:55:04 +0400

    hiding JSON objects,formatting and enveloping responses

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,10 +5,10 @@ import(
 
 type Movie struct{
 	ID int64 `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	Title string `json:"title"`
-	Year int32 `json:"year"`
-	Runtime int32 `json:"runtime"`
-	Genres []string `json:"genres"`
+	CreatedAt time.Time `json:"-"` 
+	Title string `json:"title"` 
+	Year int32 `json:"year,omitempty"` 
+	Runtime int32 `json:"runtime,omitempty"`  
+	Genres []string `json:"genres,omitempty"`  
 	Version int32 `json:"version"`
 }
