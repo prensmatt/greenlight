8e27c8641a551320c08f6826ff3be67e50fe15ae
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-21 19:14:47 +0400

    validation control

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -4,8 +4,7 @@ import(
 	"regexp"
 )
 var(
-	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.
-	[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
 type Validator struct{
 	Errors map[string]string
