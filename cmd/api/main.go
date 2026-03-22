ce57c2689e93c0ef554dfa5af5f17139fc233628
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-22 23:42:27 +0400

    correction of little syntax mistakes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,8 +36,8 @@ func main(){
 	flag.StringVar(&cfg.env,"env","development","Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn,"db-dsn",os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
-	flag.StringVar(&cfg.db.maxOpenConns,"db-max-open-conns",25, "PostgreSQL max open connections")
-	flag.StringVar(&cfg.db.maxIdleConns,"db-max-idle-conns",25, "PostgreSQL max idle connections")
+	flag.IntVar(&cfg.db.maxOpenConns,"db-max-open-conns",25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns,"db-max-idle-conns",25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime,"db-max-idle-time","15m", "PostgreSQL max connection idle time")
 
 	flag.Parse()
@@ -79,8 +79,8 @@ func openDB(cfg config)(*sql.DB,error){
 	}
 
 //
-	db.SetMaxOpenConnections(cfg.db.maxOpenConns)
-	db.SetMaxIdleConnections(cfg.db.maxIdleConns)
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
 	duration,err := time.ParseDuration(cfg.db.maxIdleTime)
 	if err != nil{
