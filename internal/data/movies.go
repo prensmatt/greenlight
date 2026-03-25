00eae0bc769f45da9e94921103a4422edcc8de63
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-25 14:17:32 +0400

    timeouts outside of postgresql

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -48,7 +48,10 @@ func(m *MovieModel) Insert(movie *Movie) error{
 	RETURNING id, created_at, version`
 
 	args := []interface{}{movie.Title,movie.Year,movie.Runtime,pq.Array(movie.Genres)}
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID,&movie.CreatedAt,&movie.Version)
+
+	ctx,cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID,&movie.CreatedAt,&movie.Version)
 }
 
 func(m *MovieModel) Get(id int64)(*Movie,error){
@@ -56,7 +59,7 @@ func(m *MovieModel) Get(id int64)(*Movie,error){
 		return nil, ErrRecordNotFound
 	}
 	query := `
-	SELECT pg_sleep(10),id, created_at, title, year, runtime, genres, version
+	SELECT id, created_at, title, year, runtime, genres, version
 	FROM movies
 	WHERE id = $1`
 
@@ -66,7 +69,6 @@ func(m *MovieModel) Get(id int64)(*Movie,error){
 	defer cancel()
 
 	err := m.DB.QueryRowContext(ctx, query, id).Scan(
-		&[]byte{},
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -94,7 +96,10 @@ func(m *MovieModel) Update(movie *Movie) error{
 	RETURNING version`
 
 	args := []interface{}{movie.Title,movie.Year,movie.Runtime,pq.Array(movie.Genres),movie.ID,movie.Version}
-	err := m.DB.QueryRow(query,args...).Scan(&movie.Version)
+
+	ctx,cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query,args...).Scan(&movie.Version)
 	if err != nil{
 		switch{
 		case errors.Is(err,sql.ErrNoRows):
@@ -114,7 +119,10 @@ func(m *MovieModel) Delete(id int64) error{
 	query := `DELETE FROM movies 
 	WHERE id =$1`
 
-	result,err := m.DB.Exec(query,id)
+	ctx, cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+
+	result,err := m.DB.ExecContext(ctx,query,id)
 	if err != nil{
 		return err
 	}
