f2fdcc3ea206a50ca7daf5b9e987407cbad752b7
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-25 14:29:41 +0400

    creating helper functions

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"net/url"
 	"fmt"
 	"io"
 	"strings"
 	"strconv"
 	
+	"greenlight.maleykaheybatova.net/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -84,4 +86,34 @@ func(app *application) readJSON(w http.ResponseWriter, r *http.Request, dst inte
 		}
 	}
 	return nil
+}
+
+
+func(app *application) readString(qs url.Values, key string, defaultValue string) string{
+	s := qs.Get(key)
+	if s == ""{
+		return defaultValue
+	}
+	return s
+}
+
+func(app *application) readCSV(qs url.Values, key string, defaultValue []string) []string{
+	csv := qs.Get(key)
+	if csv == ""{
+		return defaultValue
+	}
+	return strings.Split(csv,",")
+}
+
+func(app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int{
+	s := qs.Get(key)
+	if s == ""{
+		return defaultValue
+	}
+	i,err := strconv.Atoi(s)
+	if err != nil{
+		v.AddError(key,"must be an integer value")
+		return defaultValue
+	}
+	return i
 }
