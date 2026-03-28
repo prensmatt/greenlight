56ba58587825b6a159795522d73c06d5741ccdf9
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-28 23:11:12 +0400

    creating the UserModel, insert,getbyemail, update

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,5 +1,7 @@
 package data
 import(
+	"context"
+	"database/sql"
 	"errors"
 	"time"
 
@@ -7,13 +9,21 @@ import(
 	"golang.org/x/crypto/bcrypt"
 )
 
+var(
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+type UserModel struct{
+	DB *sql.DB
+}
+
 type User struct{
 	ID int64 `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name string `json:"name"`
 	Email string `json:"email"`
 	Password password `json:"-"`
-	Activeted bool `json:"activated"`
+	Activated bool `json:"activated"`
 	Version int `json:"-"`
 }
 
@@ -67,4 +77,82 @@ func ValidatePasswordPlainText(v *validator.Validator, password string){
 	v.Check(password != "", "password", "must be provided")
 	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
 	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+
+
+// Insert
+func(m UserModel) Insert(user *User) error{
+	query := `INSERT INTO users (name, email, password_hash, activated)
+						VALUES ($1, $2, $3, $4)
+						RETURNING id, created_at, version`
+	args := []interface{}{user.Name,user.Email,user.Password.hash,user.Activated}
+	ctx,cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx,query,args...).Scan(&user.ID,&user.CreatedAt,&user.Version)
+	if err != nil{
+		switch{
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+
+//GetByEmail
+
+func(m UserModel) GetByEmail(email string)(*User, error){
+	query := `SELECT id, created_at, name, email, password_hash, activated, version
+						FROM users
+						WHERE email = $1`
+	var user User
+	ctx,cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx,query,email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+	if err != nil{
+		switch{
+		case errors.Is(err,sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil,err
+		}
+	}
+	return &user,nil
+}
+
+
+// Update
+func(m UserModel) Update(user *User) error{
+	query := `UPDATE users
+						SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
+						WHERE id = $5 AND version = $6
+						RETURNING version`
+	args := []interface{}{user.Name,user.Email,user.Password.hash,user.Activated,user.Version}
+	ctx,cancel := context.WithTimeout(context.Background(),3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx,query,args...).Scan(&user.Version)
+	if err != nil{
+		switch{
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err,sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
