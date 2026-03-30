cfd9e2275bc01411ae23f713470eeb588cc77b0e
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-30 20:01:14 +0400

    sending activation tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,6 +2,7 @@ package main
 import(
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.maleykaheybatova.net/internal/data"
 	"greenlight.maleykaheybatova.net/internal/validator"
@@ -49,8 +50,20 @@ func(app *application) registerUserHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID,3*24*time.Hour,data.ScopeActivation)
+	if err != nil{
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
 	app.background(func(){
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl",user)
+
+		data := map[string]interface{}{
+			"activationToken": token.PlainText,
+			"userID": user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl",data)
 		if err != nil{
 			app.logger.PrintError(err,nil)
 		}
