d89802e845b86039c944209c0630e5770cd488dc
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-30 17:00:06 +0400

    background concurrent email sending for fast execution

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -49,12 +49,14 @@ func(app *application) registerUserHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl",user)
-	if err != nil{
-		app.serverErrorResponse(w,r,err)
-		return
-	}
-	err = app.writeJSON(w,http.StatusCreated, envelope{"user":user},nil)
+	go func(){
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl",user)
+		if err != nil{
+			app.logger.PrintError(err,nil)
+		}
+	}()
+
+	err = app.writeJSON(w,http.StatusAccepted, envelope{"user":user},nil)
 	if err != nil{
 		app.serverErrorResponse(w,r,err)
 	}
