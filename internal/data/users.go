a7ccd49e7607bd2f8ed557ba8488f64dd9602156
Maleyka Heybatova <6nisan2005@gmail.com>
2026-03-31 23:40:16 +0400

    authenticating requests, reading and writing to the request context

--- internal/data/users.go
+++ internal/data/users.go
@@ -18,6 +18,8 @@ type UserModel struct{
 	DB *sql.DB
 }
 
+var AnonymousUser = &User{}
+
 type User struct{
 	ID int64 `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -31,6 +33,10 @@ type User struct{
 type password struct{
 	plaintext *string
 	hash []byte
+}
+
+func(u *User) IsAnonymous() bool{
+	return u == AnonymousUser
 }
 
 func(p *password) Set(plaintextPassword string) error{
